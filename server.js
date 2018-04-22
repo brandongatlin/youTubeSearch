2dcc5c14e754535ba9130a95d93208a3efb7c10c
brandongatlin <brandongatlin1981@me.com>
2018-04-22 15:00:04 -0500

    changed server

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(express.static(__dirname));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+const PORT = process.env.PORT || 3001;
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-app.listen(port);
-console.log("server start");
